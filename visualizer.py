628ac1c019d687c95f365f4ca04c32c9449b833a
Icaro Nogueira <dedalusbi@gmail.com>
2025-11-21 09:24:04 -0300

    fim

--- visualizer.py
+++ visualizer.py
@@ -45,6 +45,8 @@ class Visualizer:
     def draw(self):
         self.screen.fill(settings.BG_COLOR)
 
+        self.draw_decision_boundary()
+
         for point in self.points:
             pygame.draw.circle(
                 self.screen,
@@ -66,6 +68,11 @@ class Visualizer:
         cart_y = (y-settings.HEIGHT/2) / (settings.HEIGHT/2)
         return cart_x, cart_y
     
+    def cartesian_to_pixel(self, cart_x, cart_y):
+        pixel_x = (cart_x*(settings.WIDTH/2)) + (settings.WIDTH/2)
+        pixel_y = (cart_y*(settings.HEIGHT/2)) + (settings.HEIGHT/2)
+        return int(pixel_x), int(pixel_y)
+    
     def train_model(self):
         for point in self.points:
 
@@ -77,3 +84,30 @@ class Visualizer:
             input_x, input_y = self.pixel_to_cartesian(pixel_x, pixel_y)
 
             self.perceptron.train(input_x, input_y, target)
+    
+    def draw_decision_boundary(self):
+         # w1*x + w2*y + b 
+        # y = mx + c
+        # y = -(w1/w2)x - (bias/w2)
+        weights = self.perceptron.weights
+        bias = self.perceptron.bias
+
+        if weights[1] == 0:
+            return
+        
+        x1=-1
+        y1=(-weights[0]*x1-bias) / weights[1]
+        x2=1
+        y2=(-weights[0]*x2-bias) / weights[1]
+
+        p1_pixel = self.cartesian_to_pixel(x1,y1)
+        p2_pixel = self.cartesian_to_pixel(x2, y2)
+
+        pygame.draw.line(
+            self.screen,
+            settings.LINE_COLOR,
+            p1_pixel,
+            p2_pixel,
+            5
+        )
+        
